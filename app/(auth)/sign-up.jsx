3ac5ef6c053a204a6bdb6c4a86ebcc7adb9ef0cd
itayven <itayventura2@gmail.com>
2024-12-16 16:47:45 +0200

    auth with db firebase

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "expo-router";
 import {
   View,
   Text,
@@ -11,6 +12,11 @@ import {
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Svg, { Path } from "react-native-svg";
+import { auth, db } from "./firebase";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getDocs, setDoc, doc, collection } from "firebase/firestore";
+import { use } from "react";
+import bcrypt from "react-native-bcrypt";
 
 export default function Signup() {
   const [name, setName] = useState("");
@@ -20,6 +26,58 @@ export default function Signup() {
   const [showPassword, setShowPassword] = useState(false);
   const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
   const [errors, setErrors] = useState({});
+
+  const handleRegister = async () => {
+    try {
+      // קבלת כל המסמכים מאוסף ה-Users
+      const querySnapshot = await getDocs(collection(db, "Users"));
+      let emailExists = false;
+
+      // בדיקת אם האימייל כבר קיים במערכת
+      querySnapshot.forEach((doc) => {
+        if (doc.data().email === email) {
+          emailExists = true;
+        }
+      });
+
+      if (emailExists) {
+        Alert.alert("שגיאה", "האימייל כבר נמצא במערכת");
+        return;
+      }
+
+      // אם האימייל לא קיים, המשך בתהליך ההרשמה
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(user);
+
+      if (user) {
+        try {
+          // הצפנת הסיסמה לפני שמירתה
+          const saltRounds = 10; // מספר הסיבובים
+          const hashedPassword = bcrypt.hashSync(password, saltRounds);
+
+          // שמירה של פרטי המשתמש ב-Firestore
+          await setDoc(doc(db, "Users", user.uid), {
+            email: user.email,
+            name: name,
+            password: hashedPassword, // שמירת הסיסמה המוצפנת
+          });
+
+          // הצגת הודעת הצלחה רק אחרי שהמשתמש נרשם והנתונים נשמרו
+          Alert.alert("נרשמת בהצלחה!");
+        } catch (docError) {
+          console.log("Error adding document: ", docError);
+          Alert.alert("שגיאה", "לא ניתן היה לשמור את הנתונים ב-Firestore");
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+      Alert.alert("שגיאה", "אירעה תקלה ברישום. נסה שוב מאוחר יותר.");
+    }
+  };
 
   const validate = () => {
     let valid = true;
@@ -57,8 +115,8 @@ export default function Signup() {
 
   const handleSubmit = () => {
     if (validate()) {
+      handleRegister();
       console.log("Form submitted", { name, email, password });
-      Alert.alert("נרשמת בהצלחה!");
     }
   };
 
@@ -104,8 +162,8 @@ export default function Signup() {
             <TextInput
               style={[
                 styles.input,
-                { textAlign: "left", writingDirection: "ltr" },
-              ]} // מיישר טקסט משמאל וה- placeholder מימין
+                { textAlign: "right", writingDirection: "ltr" },
+              ]}
               placeholder="אימייל"
               placeholderTextColor="#aaa"
               keyboardType="email-address"
@@ -120,12 +178,17 @@ export default function Signup() {
         {/* שדה סיסמה */}
         <View>
           <View style={styles.inputContainer}>
-            <Icon
-              name="lock-outline"
-              size={24}
-              color="#C6A052"
-              style={styles.iconLeft}
-            />
+            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+              <Text>
+                {" "}
+                {/* הוספתי את רכיב ה-Text כאן */}
+                <Icon
+                  name={showPassword ? "eye" : "eye-off"}
+                  size={24}
+                  color="#C6A052"
+                />
+              </Text>
+            </TouchableOpacity>
             <TextInput
               style={[styles.input, { textAlign: "right" }]}
               placeholder="סיסמה"
@@ -134,13 +197,6 @@ export default function Signup() {
               value={password}
               onChangeText={setPassword}
             />
-            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-              <Icon
-                name={showPassword ? "eye-off" : "eye"}
-                size={24}
-                color="#C6A052"
-              />
-            </TouchableOpacity>
           </View>
           {errors.password && (
             <Text style={styles.errorText}>{errors.password}</Text>
@@ -150,12 +206,19 @@ export default function Signup() {
         {/* שדה אימות סיסמה */}
         <View>
           <View style={styles.inputContainer}>
-            <Icon
-              name="lock-check"
-              size={24}
-              color="#C6A052"
-              style={styles.iconLeft}
-            />
+            <TouchableOpacity
+              onPress={() => setShowPasswordConfirm(!showPasswordConfirm)}
+            >
+              <Text>
+                {" "}
+                {/* הוספתי את רכיב ה-Text כאן */}
+                <Icon
+                  name={showPasswordConfirm ? "eye" : "eye-off"}
+                  size={24}
+                  color="#C6A052"
+                />
+              </Text>
+            </TouchableOpacity>
             <TextInput
               style={[styles.input, { textAlign: "right" }]}
               placeholder="אימות סיסמה"
@@ -164,15 +227,6 @@ export default function Signup() {
               value={passwordConfirm}
               onChangeText={setPasswordConfirm}
             />
-            <TouchableOpacity
-              onPress={() => setShowPasswordConfirm(!showPasswordConfirm)}
-            >
-              <Icon
-                name={showPasswordConfirm ? "eye-off" : "eye"}
-                size={24}
-                color="#C6A052"
-              />
-            </TouchableOpacity>
           </View>
           {errors.passwordConfirm && (
             <Text style={styles.errorText}>{errors.passwordConfirm}</Text>
@@ -186,6 +240,7 @@ export default function Signup() {
 
         {/* כפתור הרשמה דרך Google */}
         <TouchableOpacity style={styles.googleButton}>
+          <Text style={styles.googleButtonText}>הרשמה דרך Google</Text>
           <Svg width="24" height="24" viewBox="0 0 48 48">
             <Path
               d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12 c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20 c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
@@ -204,8 +259,15 @@ export default function Signup() {
               fill="#1976D2"
             />
           </Svg>
-          <Text style={styles.googleButtonText}>הרשמה דרך Google</Text>
         </TouchableOpacity>
+        <View style={styles.linkContainer}>
+          <Text style={styles.text}>יש לך כבר משתמש? </Text>
+          <Link href="/sign-in" asChild>
+            <TouchableOpacity>
+              <Text style={styles.link}> התחבר כאן</Text>
+            </TouchableOpacity>
+          </Link>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -229,7 +291,7 @@ const styles = StyleSheet.create({
     elevation: 2,
   },
   input: { flex: 1, padding: 10, fontSize: 16, color: "#333" },
-  iconLeft: { marginRight: 10 },
+
   errorText: {
     color: "#FF3D00",
     fontSize: 12,
@@ -247,7 +309,7 @@ const styles = StyleSheet.create({
   googleButton: {
     flexDirection: "row",
     alignItems: "center",
-    backgroundColor: "#333", // צבע אדום של גוגל
+    backgroundColor: "#333",
     borderRadius: 10,
     paddingVertical: 15,
     justifyContent: "center",
@@ -258,9 +320,25 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 16,
     fontWeight: "bold",
-    marginLeft: 10,
+    marginRight: 5,
   },
   googleIcon: {
     marginRight: 5,
+  },
+
+  linkContainer: {
+    marginTop: 25,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  text: {
+    fontSize: 16,
+  },
+  link: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#C6A052",
   },
 });
