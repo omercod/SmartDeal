4e784975a9675a904670c6fe894c43aa3e937e81
itayven <itayventura2@gmail.com>
2024-12-23 18:26:58 +0200

    user page+db

--- app/(main)/user-page.jsx
+++ app/(main)/user-page.jsx
@@ -10,7 +10,15 @@ import {
   ScrollView,
   TextInput,
 } from "react-native";
-import { getDocs, collection, doc, getDoc } from "firebase/firestore";
+import {
+  getDocs,
+  collection,
+  doc,
+  getDoc,
+  query,
+  where,
+  addDoc,
+} from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import Slider from "@react-native-community/slider";
 import { db } from "../(auth)/firebase";
@@ -24,6 +32,48 @@ const UserPage = () => {
   const [isOfferModalVisible, setIsOfferModalVisible] = useState(false);
   const [offerDetails, setOfferDetails] = useState({ price: "", note: "" });
   const [currentUserName, setCurrentUserName] = useState("משתמש מחובר");
+  const [selectedUserName, setSelectedUserName] = useState("");
+
+  const submitProposal = async () => {
+    if (!selectedPost || !offerDetails.price || !currentUserName) {
+      console.log("Missing required information for submission");
+      return;
+    }
+
+    try {
+      // שליפה של האימייל של המשתמש המחובר
+      const auth = getAuth();
+      const currentUser = auth.currentUser;
+
+      if (!currentUser || !currentUser.email) {
+        console.error("No authenticated user found!");
+        return;
+      }
+
+      // הכנה של המידע להצעה
+      const proposalData = {
+        jobType: selectedPost.mainCategory || "לא צוין", // סוג העבודה
+        providerName: currentUserName, // שם נותן העבודה
+        providerEmail: currentUser.email, // אימייל נותן העבודה
+        OfferPrice: offerDetails.price, // המחיר המוצע
+        note: offerDetails.note || "", // הערה
+        clientName: selectedUserName || "לא צוין", // שם הלקוח
+        clientEmail: selectedPost.userEmail || "לא צוין", // אימייל הלקוח
+        createdAt: new Date().toISOString(), // תאריך יצירת ההצעה
+        isreading: 0,
+      };
+
+      await addDoc(collection(db, "Offers"), proposalData);
+
+      console.log("Offers successfully submitted:", proposalData);
+      alert("ההצעה נשלחה בהצלחה!");
+    } catch (error) {
+      console.error("Error submitting Offers:", error);
+      alert("אירעה שגיאה בשליחת ההצעה. נסה שוב.");
+    }
+
+    closeOfferModal();
+  };
 
   useEffect(() => {
     const fetchCurrentUser = async () => {
@@ -90,8 +140,44 @@ const UserPage = () => {
     return value === priceValue;
   };
 
+  const fetchUserNameByEmail = async (email) => {
+    if (!email) {
+      console.log("Email is not available");
+      return;
+    }
+
+    try {
+      const userQuery = query(
+        collection(db, "Users"),
+        where("email", "==", email)
+      );
+      const querySnapshot = await getDocs(userQuery);
+
+      if (!querySnapshot.empty) {
+        const userDoc = querySnapshot.docs[0];
+        const userData = userDoc.data();
+        console.log(userData.name);
+        setSelectedUserName(userData.name);
+      } else {
+        console.log("No user found with this email");
+      }
+    } catch (error) {
+      console.error("Error fetching user data: ", error);
+    }
+  };
+
   const openModal = (post) => {
+    console.log("Selected post: ", post); // הדפסת הפוסט על מנת לוודא שהוא כולל את האימייל
     setSelectedPost(post);
+
+    const email = post.userEmail; // שליפת האימייל מתוך הפוסט
+
+    if (email) {
+      fetchUserNameByEmail(email); // שליחה לפונקציה לחיפוש שם המשתמש
+    } else {
+      console.log("Email is missing in post");
+    }
+
     setIsModalVisible(true);
   };
 
@@ -112,71 +198,83 @@ const UserPage = () => {
     setOfferDetails({ price: "", note: "" });
   };
 
-  // פונקציות לשליטה בתמונות
-  const images = [
-    "https://via.placeholder.com/300x200.png?text=תמונה+1",
-    "https://via.placeholder.com/300x200.png?text=תמונה+2",
-    "https://via.placeholder.com/300x200.png?text=תמונה+3",
-  ];
-
   const handleImageChange = (index) => {
     setCurrentImageIndex(index);
   };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.titletop}>דרושים:</Text>
       <FlatList
         horizontal
         data={posts}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => {
-          const priceValue = parseFloat(
-            item.price.replace("₪", "").replace(",", "")
-          );
-          const minValue = priceValue * 0.5;
-          const maxValue = priceValue * 1.2;
+          // יצירת מערך של כל התמונות כולל התמונה הראשית
+          const allImages = [item.mainImage, ...(item.additionalImages || [])];
+          const resetSliderValue = () => {
+            setSliderValues((prev) => ({
+              ...prev,
+              [item.id]: parseFloat(
+                item.price.replace("₪", "").replace(",", "")
+              ),
+            }));
+          };
 
           return (
             <View style={styles.card}>
               <View style={styles.imageContainer}>
-                <ScrollView
-                  horizontal
-                  pagingEnabled
-                  onMomentumScrollEnd={(e) => {
-                    const contentOffsetX = e.nativeEvent.contentOffset.x;
-                    const newIndex = Math.floor(contentOffsetX / 320);
-                    setCurrentImageIndex(newIndex);
-                  }}
-                  showsHorizontalScrollIndicator={false}
-                >
-                  {images.map((imageUri, index) => (
-                    <Image
-                      key={index}
-                      source={{ uri: imageUri }}
-                      style={styles.image}
-                    />
-                  ))}
-                </ScrollView>
+                {item.mainImage ? (
+                  <ScrollView
+                    horizontal
+                    pagingEnabled
+                    onMomentumScrollEnd={(e) => {
+                      const contentOffsetX = e.nativeEvent.contentOffset.x;
+                      const newIndex = Math.floor(contentOffsetX / 320); // רוחב התמונה
+                      setCurrentImageIndex(newIndex);
+                    }}
+                    showsHorizontalScrollIndicator={false}
+                  >
+                    {/* הצגת תמונות נוספות אם יש */}
+                    {[item.mainImage, ...(item.additionalImages || [])].map(
+                      (imageUri, index) => (
+                        <Image
+                          key={index}
+                          source={{ uri: imageUri }}
+                          style={styles.image}
+                        />
+                      )
+                    )}
+                  </ScrollView>
+                ) : (
+                  <View style={styles.noImageContainer}>
+                    <Text style={styles.noImageIcon}>📷</Text>
+                    <Text style={styles.noImageText}>אין תמונות</Text>
+                  </View>
+                )}
 
-                <View style={styles.dotsContainer}>
-                  {images.map((_, index) => (
-                    <TouchableOpacity
-                      key={index}
-                      onPress={() => handleImageChange(index)}
-                      style={[
-                        styles.dot,
-                        index === currentImageIndex && styles.activeDot,
-                      ]}
-                    />
-                  ))}
-                </View>
+                {/* עיגולים לתצוגת תמונות */}
+                {item.mainImage && (
+                  <View style={styles.dotsContainer}>
+                    {allImages.map((_, index) => (
+                      <TouchableOpacity
+                        key={index}
+                        onPress={() => handleImageChange(index)}
+                        style={[
+                          styles.dot,
+                          index === currentImageIndex && styles.activeDot,
+                        ]}
+                      />
+                    ))}
+                  </View>
+                )}
               </View>
 
               <Text style={styles.categoryPrimary}>{item.mainCategory}</Text>
               <Text style={styles.categorySecondary}>{item.subCategory}</Text>
               <Text style={styles.title}>{item.title}</Text>
               <Text style={styles.description}>{item.description}</Text>
-              <Text style={styles.location}>מיקום: </Text>
+              <Text style={styles.location}>מיקום: {item.city} </Text>
 
               <View style={styles.priceContainer}>
                 <Text style={styles.priceLabel}>מחיר נדרש:</Text>
@@ -187,14 +285,23 @@ const UserPage = () => {
                 <Text style={styles.sliderText}>הגש הצעה:</Text>
                 <Slider
                   style={styles.slider}
-                  minimumValue={minValue}
-                  maximumValue={maxValue}
-                  step={10}
-                  value={sliderValues[item.id]}
+                  minimumValue={Math.ceil(
+                    parseFloat(item.price.replace("₪", "").replace(",", "")) *
+                      0.5
+                  )}
+                  maximumValue={Math.floor(
+                    parseFloat(item.price.replace("₪", "").replace(",", "")) *
+                      1.2
+                  )}
+                  step={1}
+                  value={
+                    sliderValues[item.id] ||
+                    parseFloat(item.price.replace("₪", "").replace(",", ""))
+                  }
                   minimumTrackTintColor="#C6A052"
                   maximumTrackTintColor="#d3d3d3"
                   thumbTintColor="#C6A052"
-                  onValueChange={(value) => handleSliderChange(item.id, value)}
+                  onValueChange={(value) => handleSliderChange(item.id, value)} // עדכון הערך כשיש שינוי
                 />
               </View>
 
@@ -215,9 +322,17 @@ const UserPage = () => {
                   >
                     <Text style={styles.buttonText}>הגש הצעה</Text>
                   </TouchableOpacity>
+
                   <Text style={styles.sliderValue}>
                     {sliderValues[item.id]?.toFixed(2)} ₪
                   </Text>
+                  {/* כפתור איפוס */}
+                  <TouchableOpacity
+                    style={styles.resetButton}
+                    onPress={resetSliderValue}
+                  >
+                    <Text style={styles.resetButtonText}>איפוס</Text>
+                  </TouchableOpacity>
                 </View>
               )}
             </View>
@@ -225,15 +340,14 @@ const UserPage = () => {
         }}
         showsHorizontalScrollIndicator={false}
         snapToAlignment="center"
+        snapToInterval={390}
         decelerationRate="fast"
-        snapToInterval={320}
+        pagingEnabled={true}
       />
-
       {/* פופאפ - פרטי הצעת עבודה */}
       <Modal visible={isOfferModalVisible} transparent animationType="slide">
         <View style={styles.modalOverlay}>
           <View style={styles.modalContent}>
-            {/* כפתור איקס */}
             <TouchableOpacity
               style={styles.closeIcon}
               onPress={closeOfferModal}
@@ -248,7 +362,7 @@ const UserPage = () => {
                   עבודה: {selectedPost.subCategory}
                 </Text>
                 <Text style={styles.modalText}>
-                  שם נותן השירות : {currentUserName} {/* כאן מציגים את השם */}
+                  שם נותן השירות : {currentUserName}
                 </Text>
                 <Text style={styles.modalText}>
                   מחיר מוצע: {offerDetails.price} ₪
@@ -257,6 +371,8 @@ const UserPage = () => {
                   style={styles.textInput}
                   placeholder="הוסף הערה..."
                   placeholderTextColor="#C6A052"
+                  multiline={true}
+                  returnKeyType="default"
                   value={offerDetails.note}
                   onChangeText={(text) =>
                     setOfferDetails((prev) => ({ ...prev, note: text }))
@@ -268,7 +384,9 @@ const UserPage = () => {
               style={styles.closeButton}
               onPress={closeOfferModal}
             >
-              <Text style={styles.closeButtonText}>הגש הצעה ללקוח </Text>
+              <Text style={styles.closeButtonText} onPress={submitProposal}>
+                הגש הצעה ללקוח{" "}
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -278,7 +396,6 @@ const UserPage = () => {
       <Modal visible={isModalVisible} transparent animationType="slide">
         <View style={styles.modalOverlay}>
           <View style={styles.modalContent}>
-            {/* כפתור איקס */}
             <TouchableOpacity style={styles.closeIcon} onPress={closeModal}>
               <Text style={styles.closeIconText}>×</Text>
             </TouchableOpacity>
@@ -287,7 +404,7 @@ const UserPage = () => {
             {selectedPost && (
               <>
                 <Text style={styles.modalText}>
-                  שם מציע העבודה: {selectedPost.title}
+                  שם הלקוח: {selectedUserName}
                 </Text>
                 <Text style={styles.modalText}>פלאפון: 050-1234567</Text>
               </>
@@ -301,33 +418,31 @@ const UserPage = () => {
     </ScrollView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f9f9f9", // צבע הרקע של הקונטיינר
+    backgroundColor: "#f9f9f9",
     justifyContent: "center",
     alignItems: "center",
     paddingBottom: 20,
   },
-  modalOverlay: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)", // צבע הרקע השקוף למחצה של ה-modal
+  titletop: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#C6A052",
+    marginTop: 20,
+    marginBottom: 20,
+    right: 150,
+    top: 90,
+    borderBottomWidth: 2,
+    borderBottomColor: "#C6A052",
   },
-  modalContent: {
-    width: "80%",
-    backgroundColor: "white", // צבע הרקע של התוכן בתוך ה-modal
-    borderRadius: 15,
-    padding: 20,
-    alignItems: "center",
-  },
-
   card: {
     marginTop: 100,
-    width: 320,
-    height: 620,
-    marginHorizontal: 15,
+    width: 350,
+    height: 540,
+    marginHorizontal: 20,
     backgroundColor: "white",
     borderRadius: 15,
     padding: 20,
@@ -337,18 +452,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 10 },
     justifyContent: "space-between",
     alignItems: "center",
-    elevation: 5,
+    elevation: 20,
     borderWidth: 1,
     borderColor: "#ddd",
+    overflow: "hidden",
   },
   imageContainer: {
-    width: "100%",
-    height: 180,
-    borderWidth: 2,
-    borderColor: "#C6A052",
-    borderRadius: 15,
+    width: 320,
+    height: 210,
+
     overflow: "hidden",
-    marginBottom: 10,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "white",
@@ -361,60 +474,61 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "bold",
     color: "#C6A052",
-    marginBottom: 5,
+    marginTop: 10,
     textTransform: "uppercase",
   },
   categorySecondary: {
     fontSize: 12,
     color: "#888",
-    marginBottom: 5,
+    marginTop: 10,
     fontStyle: "italic",
   },
   title: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: "bold",
-    marginBottom: 5,
+    marginTop: 10,
     color: "#333",
     textAlign: "center",
   },
   description: {
-    fontSize: 14,
+    fontSize: 12,
     color: "#555",
-    marginBottom: 10,
+    marginTop: 10,
     textAlign: "center",
+    flexShrink: 1,
+    overflow: "hidden",
   },
   location: {
-    fontSize: 14,
+    fontSize: 12,
     color: "#555",
-    marginBottom: 10,
+    marginTop: 10,
     textAlign: "center",
   },
   priceContainer: {
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 10,
+    marginTop: 10,
   },
   priceLabel: {
-    fontSize: 16,
+    fontSize: 14,
     color: "#C6A052",
     fontWeight: "bold",
     marginRight: 5,
   },
   price: {
-    fontSize: 16,
+    fontSize: 14,
     color: "#333",
     fontWeight: "bold",
   },
   sliderContainer: {
     alignItems: "center",
-    marginBottom: 20,
   },
   sliderText: {
     fontSize: 14,
     fontWeight: "bold",
     color: "#C6A052",
-    marginBottom: 10,
+    marginTop: 10,
   },
   slider: {
     width: 280,
@@ -486,7 +600,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     position: "absolute",
-    bottom: 10,
+    bottom: 0,
+    borderColor: "#C6A052",
   },
   dot: {
     width: 10,
@@ -494,12 +609,15 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     backgroundColor: "#ffff",
     margin: 5,
+    borderWidth: 2,
+    borderColor: "#C6A052",
   },
   activeDot: {
     backgroundColor: "#C6A052",
+    borderColor: "#C6A052",
   },
   textInput: {
-    height: 40,
+    height: 100,
     borderColor: "#ccc",
     borderWidth: 1,
     borderRadius: 10,
@@ -507,12 +625,13 @@ const styles = StyleSheet.create({
     marginTop: 10,
     width: "100%",
     textAlign: "right",
+    paddingBottom: 60,
   },
   closeIcon: {
     position: "absolute",
     top: 10,
     left: 10,
-    zIndex: 1, // להבטיח שהאייקון יישאר מעל כל אלמנט אחר
+    zIndex: 1,
     backgroundColor: "transparent",
     padding: 5,
   },
@@ -520,6 +639,40 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "bold",
     color: "#C6A052",
+  },
+  noImageContainer: {
+    width: 320,
+    height: 180,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ddd",
+  },
+  noImageIcon: {
+    fontSize: 40,
+    color: "#888",
+  },
+  noImageText: {
+    marginTop: 10,
+    fontSize: 16,
+    color: "#C6A052",
+    fontWeight: "bold",
+  },
+  resetButton: {
+    backgroundColor: "#333",
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  resetButtonText: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "bold",
   },
 });
 
